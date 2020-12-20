fd88f44fb2b1bda583cd89dbdedc32e9711fdc96
Dan Langille <dan@langille.org>
2020-12-20 22:26:33 +0000

    what?
    git-svn-id: svn://127.0.0.16/freshports-1/database-schema@5542 46e82423-29d8-e211-989e-002590a4cdd4

--- branches/git/updates-2020-12-git-changes.sql
+++ branches/git/updates-2020-12-git-changes.sql
@@ -1,6 +1,3 @@
-insert into repo (name, description, repo_hostname, path_to_repo, repository) values
-('src', 'The FreeBSD src tree', 'github.com', '/freebsd/freebsd', 'git');
-insert into repo (name, description, repo_hostname, path_to_repo, repository) values
-('doc', 'The FreeBSD doc tree', 'github.com', '/freebsd/doc', 'git');
-insert into repo (name, description, repo_hostname, path_to_repo, repository) values
-('ports', 'The FreeBSD Ports tree', 'github.com', '/freebsd/freebsd-ports', 'git');
+insert into repo (name, description, repo_hostname, path_to_repo, repository) values ('ports', 'The FreeBSD Ports tree', 'github.com', '/freebsd/freebsd-ports', 'git');
+insert into repo (name, description, repo_hostname, path_to_repo, repository) values ('doc', 'The FreeBSD doc tree', 'github.com', '/freebsd/doc', 'git');
+insert into repo (name, description, repo_hostname, path_to_repo, repository) values ('src', 'The FreeBSD src tree', 'github.com', '/freebsd/freebsd', 'git');
