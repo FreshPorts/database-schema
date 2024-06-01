806daf9e8630c26d4c242745d82e1f0824b9a473
Dan Langille <dan@langille.org>
2024-06-01 12:51:45 +0000

    Allow reading to select from watch_list_element
    re: https://github.com/FreshPorts/freshports/issues/550
    
    
    git-svn-id: svn://127.0.0.16/freshports-1/database-schema@6058 46e82423-29d8-e211-989e-002590a4cdd4

--- branches/git/permissions.sql
+++ branches/git/permissions.sql
@@ -350,6 +350,7 @@ GRANT SELECT                 ON announcements            to reading;
 GRANT SELECT                 ON package_notifications    to reading;
 GRANT SELECT                 ON report_subscriptions_abi to reading;
 GRANT SELECT                 ON abi                      to reading;
+GRANT SELECT                 ON watch_list_element       to reading;
 GRANT TRUNCATE               ON package_notifications    to packaging;
 GRANT INSERT                 ON report_log_package_notifications        TO reading;
 GRANT SELECT, UPDATE         ON report_log_package_notifications_id_seq TO reading;
